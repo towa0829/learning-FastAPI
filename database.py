095eb92b9fe35dd53f662d383f4229c8f00397da
towa0829 <towa0000829@icloud.com>
2026-06-14 22:07:14 +0900

    feat: create db

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# データベースファイルへの接続経路を作る
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+engine = create_engine(
+  SQLALCHEMY_DATABASE_URL,
+  connect_args = {"check_same_thread": False}
+)
+# セッションを作るための型
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# モデル（テーブル定義）の土台となるクラス
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from database import engine, SessionLocal
+import models
+
+# データベーステーブルを作成
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-todos = []
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
 # 型定義
 class Todo(BaseModel):
@@ -15,14 +26,17 @@ def read_root():
   return {"message": "Hello, World!"}
 
 @app.get("/todos")
-def get_todos():
-  return todos
+def get_todos(db: Session = Depends(get_db)):
+  return db.query(models.Todo).all()
 
 @app.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-  return todos[todo_id]
+def get_todo(todo_id: int, db: Session = Depends(get_db)):
+  return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
 
 @app.post("/todos")
-def create_todo(todo: Todo):
-  todos.append(todo)
-  return todo
+def create_todo(todo: Todo, db: Session = Depends(get_db)):
+  db_todo = models.Todo(title=todo.title, done=todo.done)
+  db.add(db_todo)
+  db.commit()
+  db.refresh(db_todo)
+  return db_todo
