34b08212f4364e25ab3ab12d35a02e116c72d332
towa0829 <towa0000829@icloud.com>
2026-06-14 21:36:52 +0900

    feat: add api for todo app

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+todos = []
+
+# 型定義
+class Todo(BaseModel):
+  title: str
+  done: bool = False
 
 @app.get("/")
 def read_root():
   return {"message": "Hello, World!"}
+
+@app.get("/todos")
+def get_todos():
+  return todos
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+  return todos[todo_id]
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+  todos.append(todo)
+  return todo
